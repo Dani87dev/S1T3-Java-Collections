794a2c8999cacf55f5eeda396ab8270bffdb2226
Daniel Vilà <danivila.dev@gmail.com>
2025-12-17 13:58:05 +0100

    feat: Level 1, Task 1: Create initial classes

---
+++ src/main/java/n1exercice1/Main.java
@@ -0,0 +1,10 @@
+package n1exercice1;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/n1exercice1/MonthManager.java
@@ -0,0 +1,43 @@
+package n1exercice1;
+
+import java.util.ArrayList;
+
+
+
+public class MonthManager {
+
+    static ArrayList<Month> calendar = new ArrayList<Month>();
+
+    public void createCalendar() {
+
+        Month January = new Month("January");
+        Month February = new Month("February");
+        Month March = new Month("March");
+        Month April = new Month("April");
+        Month May = new Month("May");
+        Month June = new Month("June");
+        Month July = new Month("July");
+        Month September = new Month("September");
+        Month October = new Month("October");
+        Month November = new Month("November");
+        Month December = new Month("December");
+
+        calendar.add(January);
+        calendar.add(February);
+        calendar.add(March);
+        calendar.add(April);
+        calendar.add(May);
+        calendar.add(June);
+        calendar.add(July);
+        calendar.add(September);
+        calendar.add(October);
+        calendar.add(November);
+        calendar.add(December);
+    }
+}
+
+
+
+
+
+
