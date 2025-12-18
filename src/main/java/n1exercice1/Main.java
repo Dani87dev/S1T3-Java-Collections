eb5421d7579abb3f192fa597ca8e4bc2ecbfd12f
Daniel Vilà <danivila.dev@gmail.com>
2025-12-18 12:58:55 +0100

    feat: Level 1 Task 1 - implement iterator method

--- src/main/java/n1exercice1/Main.java
+++ src/main/java/n1exercice1/Main.java
@@ -3,6 +3,8 @@ package n1exercice1;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+
 import static n1exercice1.MonthManager.calendar;
 
 
@@ -10,8 +12,10 @@ public class Main {
 
     public static void main (String[] args) {
 
-        tryArrayList();
-        convertArrayListToHashSet();
+        //tryArrayList();
+        //convertArrayListToHashSet();
+        //iterateWithFor();
+        iterateWithIterator();
 
     }
 
@@ -26,6 +30,7 @@ public class Main {
         System.out.println(calendar);
     }
 
+    //canviar el metodo para que reciba el arraylist como parametro y devuelva el HashSet
     static public void convertArrayListToHashSet() {
 
         MonthManager gestor2 = new MonthManager();
@@ -39,6 +44,36 @@ public class Main {
         HashSet<Month> myHashSet = new HashSet<Month>(calendar);
         System.out.println(myHashSet);
 
+    }
+
+    // escoger solo un for
+    static void iterateWithFor() {
+
+        MonthManager gestor3 = new MonthManager();
+        gestor3.createCalendar();
+
+        System.out.println("Iterating over a collection with a classic for loop");
+        for(int i =0; i<calendar.size(); i++) {
+            System.out.println(calendar.get(i));
+        }
+
+        System.out.println("Iterating over a collection with a for each loop");
+        for(Month element: calendar) {
+            System.out.println(element);
+        }
+    }
+
+    static void iterateWithIterator() {
+
+        MonthManager gestor4 = new MonthManager();
+        gestor4.createCalendar();
+        Iterator<Month> iteradorDani = calendar.iterator();
+        /*nomes imprimeix el primer
+        System.out.println(iteradorDani.next());
+        */
+        while(iteradorDani.hasNext()) {
+            System.out.println(iteradorDani.next());
+        }
     }
 
 }
