a88686154f287f2b05fae25bb0fdd103e869545f
Daniel Vilà <danivila.dev@gmail.com>
2025-12-18 12:18:33 +0100

    feat:  implement main class

--- src/main/java/n1exercice1/Main.java
+++ src/main/java/n1exercice1/Main.java
@@ -1,10 +1,26 @@
 package n1exercice1;
 
+import static n1exercice1.MonthManager.calendar;
+
 public class Main {
 
     public static void main (String[] args) {
 
+        tryArrayList();
+
+    }
+
+    static public void tryArrayList() {
+
+        MonthManager gestor = new MonthManager();
+        gestor.createCalendar();
+        System.out.println(calendar);
+
+        Month august = new Month("August");
+        calendar.add(7, august);
+        System.out.println(calendar);
 
 
     }
+
 }
